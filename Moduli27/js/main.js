64fa923e34317081ba8bb63cd6751413615b13bd
Mali78688 <lamshala8@gmail.com>
2025-04-02 11:48:19 -0700

    mod27

---
+++ Moduli27/js/main.js
@@ -0,0 +1,24 @@
+
+
+// function showMessage() {
+// alert("Hello")
+
+// }
+function sum(num1,num2) {
+return num1 + num2;
+
+}
+console.log(sum(25,5))
+
+function dsFunction() {
+var localVar= "Shkolla Digitale"
+alert(localVar);
+}
+// showMessage();
+dsFunction();
+function toSeconds(minutes) {
+
+return minutes * 60;
+
+}
+console.log(toSeconds(60))
