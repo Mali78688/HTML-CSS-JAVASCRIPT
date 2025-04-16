5bf82f08f53506bb80e74e3413646a2a0efb3934
Mali78688 <lamshala8@gmail.com>
2025-04-16 11:48:39 -0700

    moduli28

---
+++ moduli28/main.js
@@ -0,0 +1,55 @@
+var button1 = document.getElementById('btn1');
+var button2 = document.getElementById('btn2');
+var button3 = document.getElementById('btn3');
+var button4 = document.getElementById('btn4');
+var v_text = document.getElementById('txt1');
+
+
+button1.onclick = function() {
+
+alert('hello');
+}
+button2.onmouseover = function() {
+
+    alert('hello');
+    }
+    button3.onmouseleave = function() {
+
+        alert('leave');
+        }
+        button4.onmousewheel = function() {
+
+            alert('leave');
+            }
+
+
+
+v_text.onclick = function() {
+
+v_text.style.color= 'red';
+v_text.style.background= 'black';
+v_text.style.textAlign= 'center';
+
+
+}  
+
+
+
+// var firstButton = document.querySelector('button');
+
+// firstButton.onclick = fuction() {
+// alert('hello world');
+
+// }
+
+
+// button3.addEventListener('click',fuction() {
+
+
+// alert('helloworld');
+
+
+
+// }
+
+// )
