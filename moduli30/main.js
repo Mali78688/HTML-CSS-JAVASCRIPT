b430ab0607360fa8594c2aaf3d58a6e4fb7ce7a8
Mali78688 <lamshala8@gmail.com>
2025-04-28 11:50:54 -0700

    MODULI30

---
+++ moduli30/main.js
@@ -0,0 +1,15 @@
+// if(x > 2){
+//  console.log("x o ma i madh se vlera 2");
+// }
+// else{
+//     console.log("x o ma i vogel se vlera 2")
+//    }
+
+
+   var input = document.getElementById('input.id')
+   var button = document.getElementById('btn.id')
+   var text = document.getElementById('text.id')
+
+   button.onclick = fuction(){
+    text.innerHTML = input.value;
+   }
