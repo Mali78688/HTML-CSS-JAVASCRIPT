c1a10f6c5896ac50bd9dd05e640ee6f39f5f0f6d
Mali78688 <lamshala8@gmail.com>
2025-06-04 11:36:46 -0700

    38

---
+++ moduli35/slideshow/search/js/main.js
@@ -0,0 +1,13 @@
+function search(){
+    var input, filter, ul, li, a, i, txtValue;
+}
+input = document.getElementById("myInput");
+
+filter = input.value.toUpperCase();
+ul = document.getElementById("myList");
+filter = input.value.toLowerCase();
+ul = document.getElementsByTagName("myList");
+
+for (i=0; i<li.lenght; i++){
+    a = li[i]
+}
